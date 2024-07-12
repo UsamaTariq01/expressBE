c053ea5c7d5d23b11892841409483fc99445bd34
UsamaBinTariq01 <usama.tariq@moonlabor.com>
2024-07-12 12:24:06 +0500

    basics done without any CRUD

--- basics/app.js
+++ basics/app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
 const {initConfiguration} = require('./configs/config.global');
-
+const cors = require('cors');
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+const options = {
+  'origin':               '*',
+  'methods':              'GET,PUT,POST,DELETE',
+  'preflightContinue':    false,
+  'optionsSuccessStatus': 206,
+  'exposedHeaders':       'Access-Control-Allow-Method,Access-Control-Allow-Origin,Content-Type,Content-Length'
+
+};
+
+app.use(cors(options));
+
 // Route for GET request
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get('/test', (req, res) => {
+  res.send({message:'Hello, World!',status:200});
 });
 initConfiguration();
 // Start the server
--- basics/configs/config.global.js
+++ basics/configs/config.global.js
@@ -8,10 +8,9 @@ global.config={}
 const initConfiguration=()=>{
 
     try{
-        console.log(' @@ CAALLLEED : ',__dirname,'=====',__filename);
+        
         global.config = require(path.join(__dirname, 'env', `env.${environment}.json`));
-        console.log(' @@ INIT :L',global.config);
-
+        
     }catch(err){
         console.log(err);
         // throw new Error({message:'err while loading init configurations'});
